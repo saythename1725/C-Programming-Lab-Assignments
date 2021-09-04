7da9cb4c3a546e70798f36b0671555b9077fc921
Mohini Satarupa Jena <mohinisatarupajena@gmail.com>
2021-09-04 20:04:26 +0530

    Update code 3.cpp

--- code 3.cpp
+++ code 3.cpp
@@ -4,7 +4,7 @@ int main()
 {
     int a,b,gcd,i;
     printf("Enter two positive numbers which are separated by a space: \n");
-    scanf("%d%d",&a,&b);
+    scanf("%d %d",&a,&b);
     for(i=1;i<=a;i++)
     {
         if(a%i==0 && b%i==0)
