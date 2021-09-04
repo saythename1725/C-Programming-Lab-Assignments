ab7dc29ff6014ec0fa2a2055499d5d9750f9d4c6
Mohini Satarupa Jena <mohinisatarupajena@gmail.com>
2021-09-04 21:38:12 +0530

    Add files via upload

---
+++ code 2.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j,isPrime;
+    printf("Enter a positive integer:\n");
+    scanf("%d",&n);
+    if(n==1)
+	{
+		printf("1 is neither prime nor composite");
+		return 0;
+    }
+    for(i=2;i<=n/2;i++)
+    {
+        if(n%i==0)
+        {
+        break;
+        }
+    }
+        if(i>n/2)
+        printf("%d is a Prime Number\n",n);
+        else
+        {
+            printf("%d is not a prime number and the prime factors are: \n",n);
+                for(i=2; i<=n; i++)
+    {
+        if(n%i==0)
+        {
+            isPrime = 1;
+            for(j=2; j<=i/2; j++)
+            {
+                if(i%j==0)
+                {
+                    isPrime = 0;
+                    break;
+                }
+            }
+            if(isPrime==1)
+            {
+                printf("%d, ", i);
+            }
+        }
+    }
+        }
+        return 0;
+}
